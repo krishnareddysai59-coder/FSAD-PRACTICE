a4a7ee6f9fb5dfd8d1fc143760c64c4df6a39902
krishnareddysai59-coder <krishnareddysai59@gmail.com>
2026-01-21 10:56:15 +0530

    21/01/2026

---
+++ DI-ECommerce-Autowired/src/main/java/com/klu/main/MainApp.java
@@ -0,0 +1,28 @@
+package com.klu.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.klu.config.AppConfig;
+import com.klu.model.Order;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Order order = context.getBean(Order.class);
+        order.display();
+
+       
+
+        ApplicationContext context1 =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Order order1 = context1.getBean(Order.class);
+        order1.display();
+    }
+}
